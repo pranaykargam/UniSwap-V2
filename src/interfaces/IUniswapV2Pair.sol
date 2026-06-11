a79d4345aa44f94e1e0955f73c515fc38746693c
pranaykargam <sunnykargam@gmail.com>
2026-06-11 10:21:39 +0530

    updated library

--- src/interfaces/IUniswapV2Pair.sol
+++ src/interfaces/IUniswapV2Pair.sol
@@ -16,5 +16,8 @@ interface IUniswapV2Pair {
         bytes32 s
     ) external;
 
- 
+    function getReserves()
+        external
+        view
+        returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
 }
